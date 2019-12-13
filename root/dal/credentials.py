6ddc514e10e677a196d5b71e51ef3d263ff945c5
MaximKM-62 <37383153+MaximKM-62@users.noreply.github.com>
2019-12-13 23:19:00 +0200

    Update credentials.py

--- root/dal/credentials.py
+++ root/dal/credentials.py
@@ -1,9 +1,14 @@
-import os
-BASE = 'postgresql-animated-67194'
+BASE = 'postgresql+psycopg2'
 USERNAME = 'tzxlafwyaqqlzf'
 PASSWORD = '2cd44532fb7672b16eafda82016b90af320812680c97d421e763af28bc15e034'
 HOST = 'ec2-54-75-235-28.eu-west-1.compute.amazonaws.com'
 PORT = '5432'
 DATABASE = 'd7q78cmhi891kf'
-DATABASE_URI = os.getenv('postgres://tzxlafwyaqqlzf:2cd44532fb7672b16eafda82016b90af320812680c97d421e763af28bc15e034@ec2-54-75-235-28.eu-west-1.compute.amazonaws.com:5432/d7q78cmhi891kf')
-
+DATABASE_URI = '{base}://{user}:{pw}@{host}:{port}/{db}'.format(
+base=BASE,
+user=USERNAME,
+pw=PASSWORD,
+host=HOST,
+port=PORT,
+db=DATABASE
+)
